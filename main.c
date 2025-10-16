7f0c769e6cf280f5fd5334c8effc6ed5cb5c09a6
jiy0ungkim <2626kjy@naver.com>
2025-10-16 15:37:04 +0900

    03

--- main.c
+++ main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+int inc(int counter);
 
 int main(int argc, char *argv[])
 {
-    int i;
-
-    for (i=0; i<5; i++)
-    {
-        static temp = 1;
-        printf("temp = %d\n", temp);
-        temp++;
-    }
-
+    int i=10;
+    
+    printf("함수 호출 전 i = %d\n", i);
+    
+    i = inc(i);
+    //inc(i); 
+    
+    printf("함수 호출 후 i = %d\n", i);
+    
     system("PAUSE");	
     return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
